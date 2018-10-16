c35e7945ebc4a0781a6f523d3cb3304cb360d008
a0vladimirovich0k <a0vladimirovich0k@gmail.com>
2018-10-16 18:51:35 +0300

    init2

--- PapFootPath/CoreDataStack.swift
+++ PapFootPath/CoreDataStack.swift
@@ -15,7 +15,7 @@ class CoreDataStack {
 
 lazy var persistentContainer: NSPersistentContainer = {
     
-    let container = NSPersistentContainer(name: "PapFootPath")
+    let container = NSPersistentContainer(name: "Model")
     container.loadPersistentStores(completionHandler: { (storeDescription, error) in
         if let error = error as NSError? {
             // Replace this implementation with code to handle the error appropriately.
